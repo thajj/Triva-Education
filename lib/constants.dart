923e46a75da6d0c856d978e0ba4c88f46d41773e
2fik <hajj.toufic@gmail.com>
2020-04-23 23:22:18 -0400

    Remove specific code from the other other branch

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,4 @@
 class Constants {
-  String apiURL = "http://192.168.2.11:8888/quizadmin/public/";
-  String storageUrl = "http://192.168.2.11:8888/quizadmin/storage/app/public/";
-  int defaultCount = 10; // No of questions per attempt
-  String shareText = "TODO";
-  String playStoreURL = "http://play.google.com";
-  String appStoreURL = "https://apple.com";
-
   String correctSound = 'sounds/correct.mp3';
   String incorrectSound = 'sounds/incorrect.mp3';
   String btnTap = 'sounds/click.mp3';
